82e51a8023dd3e9626837876e528203601959b0e
DanielleMalingue <125500654+DanielleMalingue@users.noreply.github.com>
2026-05-25 13:06:35 -0700

    layout update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const typedStatement = document.getElementById("typedStatement");
 
 const statement =
-  "I blend design, technology, and business to solve real human problems for social good.";
+  "I blend design, technology, and business to solve real human problems.";
 
 let charIndex = 0;
 
@@ -11,7 +11,7 @@ function typeHeroStatement() {
   if (charIndex < statement.length) {
     typedStatement.textContent += statement.charAt(charIndex);
     charIndex++;
-    setTimeout(typeHeroStatement, 42);
+    setTimeout(typeHeroStatement, 52);
   }
 }
 
