df3cb3461b63926d73e917b248d78779d101884c
DanielleMalingue <125500654+DanielleMalingue@users.noreply.github.com>
2026-05-26 09:21:16 -0700

    city instead

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ function typeHeroStatement() {
 }
 
 window.addEventListener("load", typeHeroStatement);
+
+window.addEventListener("scroll", () => {
+
+    const scrollY = window.scrollY;
+  
+    document.querySelector(".skyline-back").style.transform =
+      `translateY(${scrollY * 0.04}px)`;
+  
+    document.querySelector(".skyline-front").style.transform =
+      `translateY(${scrollY * 0.08}px)`;
+  
+    document.querySelectorAll(".light").forEach((light, index) => {
+      light.style.transform =
+        `translateY(${scrollY * (0.03 + index * 0.02)}px)`;
+    });
+  
+  });
